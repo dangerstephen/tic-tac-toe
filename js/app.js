1df8376edc066e1ade6ed297dac53e630690e5e2
DANGERSTEPHEN <DANGERSTEVE1@GMAILCOM>
2016-10-30 11:23:04 -0700

    made a tic tac toe button git status git status
    cd

--- js/app.js
+++ js/app.js
@@ -2,5 +2,6 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
-
+var cells = $(".box").on('click', function(){console.log("Ive been clicked")})
 });
+console.log("Hello, I'm linked");
