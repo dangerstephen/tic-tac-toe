db4ec6b381bee64c6adbf6a5377a346454bd403e
DANGERSTEPHEN <DANGERSTEVE1@GMAILCOM>
2016-10-30 12:13:55 -0700

    started to compress functions

--- js/app.js
+++ js/app.js
@@ -57,38 +57,10 @@ function isGameOver(){
 }
 }
 
-//function to determine draw
-// // function isGameDraw(){
-// //   while (l>=9) {
-// //   alert("Game is a draw");
-// // }
-// // }
-
-//try to make an array
-
-// // If any of these patters of board spaces have all X's or all O's somebody won!
-// wins: [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]],
-//
-// /* Check for whether someone won the game of it was a Cat's game. */
-// function () {
-//
-//     // Loop through all possible winning combinations
-//     for (k in this.wins) {
-//         var pattern = this.wins[k];
-//         var p = this.board[pattern[0]] + this.board[pattern[1]] + this.board[pattern[2]];
-//         if (p == "XXX") {
-//             alert("X");  // X Won!
-//         } else if (p == "OOO") {
-//             alert( "O");  // O Won!
-//         }
-//     }
-//
-//     // Check if all spaces in the board are filled, then its a Cat's game
-//     var cnt = 0;
-//     for (s in this.board) {
-//         if (this.board[s]) { cnt += 1; }
-//     }
-//     if (cnt == 9) {
-//         return "Cat";  // Cat's game!
-//     }
+// // function to determine draw
+// function isGameDraw(){
+//   while (this>=9) {
+//   alert("Game is a draw");
 // }
+// }
+//
